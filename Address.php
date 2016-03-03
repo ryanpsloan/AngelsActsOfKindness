d4d564406f1c37c7d99ff58688bef89fd11bc880
ryanpsloan <ryansdeal@hotmail.com>
2016-03-02 18:33:23 -0700

    Zip Class - started Class - finished constructor/properties/getMethods

--- Address.php
+++ Address.php
@@ -441,6 +441,59 @@ class Address
 
     }
 
+}
+
+class ZipCode{
+
+    /**
+     *zip id for ZipCode, primary key
+     */
+
+    private $zipId;
+
+    /**
+     *zipCode field
+     *
+     */
+
+    private $zipCode;
+
+    /**
+     * ZipCode constructor.
+     * @param mixed $newZipId or null if new object
+     * @param string $newZipCode
+     * @throws UnexpectedValueException when a parameter is the wrong type
+     *
+     */
+
+    public function __construct($newZipId, $newZipCode){
+        try{
+            $this->setZipId($newZipId);
+            $this->setZipCode($newZipCode);
+
+        }catch(UnexpectedValueException $unexpectedValue){
+            throw(new UnexpectedValueException("Unable to Construct ZipCode",0,$unexpectedValue));
+        }
+    }
+
+    /***
+     * gets the value of zipId
+     *
+     * @return mixed int or null
+     */
+    public function getZipId(){
+        return $this->zipId;
+    }
+
+    /**
+     * gets the value of zipCode
+     *
+     * @return string
+     */
+    public function getZipCode(){
+        return $this->zipCode;
+    }
+
 
 
 }
